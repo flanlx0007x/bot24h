96cdb80830c536ee94efe8a74add97c1f49c1b4a
Responsive <104904660+flanlx0007x@users.noreply.github.com>
2024-06-12 20:07:40 +0700

    Update sever.py

--- sever.py
+++ sever.py
@@ -1,15 +1,11 @@
-from flask import Flask 
+from flask import Flask,render_template
 from threading import Thread
-
-app = Flask('')
-
+app = Flask(__name__)
 @app.route('/')
-def home():
-    return "Server is work"
-
+def index():
+    return "Alive"
 def run():
-    app.run(host='0.0.0.0', port=5500)
-
-def keep_alive():
+  app.run(host='0.0.0.0',port=8080)
+def keep_alive():  
     t = Thread(target=run)
-    t.start
+    t.start()
